91c7f690e1126b6019fdbfa85c42b506e248ebfa
Jeremy Friesner <jaf@meyersound.com>
2022-12-05 21:35:05 -0800

    Removed errneous const

--- system/DetectNetworkConfigChangesSession.cpp
+++ system/DetectNetworkConfigChangesSession.cpp
@@ -855,7 +855,7 @@ io_status_t DetectNetworkConfigChangesSession :: DoInput(AbstractGatewayMessageR
    struct iovec iov = {buf, sizeof(buf)};
    struct sockaddr_nl sa;
    struct msghdr msg = {(void *)&sa, sizeof(sa), &iov, 1, NULL, 0, 0 };
-   const int msgLen = recvmsg(fd, &msg, 0);
+   int msgLen = recvmsg(fd, &msg, 0);
    if (msgLen >= 0)  // FogBugz #9620
    {
       for (struct nlmsghdr *nh = (struct nlmsghdr *)buf; NLMSG_OK(nh, (unsigned int)msgLen); nh=NLMSG_NEXT(nh, msgLen))
