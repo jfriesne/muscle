ea4a8a1f9b64506ebdabda7fda52148631a4a5a8
Jeremy Friesner <jaf@meyersound.com>
2022-12-31 14:23:03 -0800

    Added a #ifndef DOXYGEN_AVOID_MUSCLE_MAINPAGE_TAG [...] #endif into MuscleSupport.h so that MUSCLE docs can be generated as part of a calling project, without causing a duplicate-mainpage error

--- support/MuscleSupport.h
+++ support/MuscleSupport.h
@@ -14,6 +14,8 @@
 #define MUSCLE_VERSION_STRING "9.20" /**< The current version of the MUSCLE distribution, expressed as an ASCII string */
 #define MUSCLE_VERSION        92000  /**< Current version, expressed as decimal Mmmbb, where (M) is the number before the decimal point, (mm) is the number after the decimal point, and (bb) is reserved */
 
+#ifndef DOXYGEN_AVOID_MUSCLE_MAINPAGE_TAG
+
 /*! \mainpage MUSCLE Documentation Page
  *
  * The MUSCLE API provides a robust, efficient, scalable, cross-platform, client-server messaging system
@@ -39,6 +41,8 @@
  * you'll want to write your own client software.  Example client software can be found in the "test"
  * subdirectory.
  */
+
+#endif
 
 #include <assert.h>  /* for assert() */
 #include <string.h>  /* for memcpy() */
