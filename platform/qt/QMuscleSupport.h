ed07c9b0d1b675ef61e924c44d471db59a2c7475
Jeremy Friesner <jaf@meyersound.com>
2023-06-08 17:36:57 -0700

    Added hash functors for QPoint and QRect

--- platform/qt/QMuscleSupport.h
+++ platform/qt/QMuscleSupport.h
@@ -4,6 +4,8 @@
 #define QMuscleSupport_h
 
 #include <QString>
+#include <QPoint>
+#include <QRect>
 
 #include "support/MuscleSupport.h"  // for PODHashFunctor, etc
 
@@ -16,8 +18,7 @@
 namespace muscle {
 
 /** Enables the use of QStrings as keys in a MUSCLE Hashtable. */
-template <>
-class PODHashFunctor<QString>
+template <> class PODHashFunctor<QString>
 {
 public:
    /** Returns a hash code for the given QString object.
@@ -38,6 +39,50 @@ public:
      * @param k2 second key to compare
      */
    MUSCLE_NODISCARD bool AreKeysEqual(const QString & k1, const QString & k2) const {return (k1==k2);}
+};
+
+/** Enables the use of QPoints as keys in a MUSCLE Hashtable. */
+template <> class PODHashFunctor<QPoint>
+{
+public:
+   /** Returns a hash code for the given QPoint object.
+     * @param pt The QPoint to calculate a hash code for.
+     */
+   MUSCLE_NODISCARD uint32 operator () (const QPoint & pt) const
+   {
+      const uint32 hx = CalculateHashCode(pt.x());
+      const uint32 hy = CalculateHashCode(pt.y());
+      return (hx + (hy*13));
+   }
+
+   /** Returns true iff the two QPoint are equal.
+     * @param k1 first key to compare
+     * @param k2 second key to compare
+     */
+   MUSCLE_NODISCARD bool AreKeysEqual(const QPoint & k1, const QPoint & k2) const {return (k1==k2);}
+};
+
+/** Enables the use of QRects as keys in a MUSCLE Hashtable. */
+template <> class PODHashFunctor<QRect>
+{
+public:
+   /** Returns a hash code for the given QRect object.
+     * @param r The QRect to calculate a hash code for.
+     */
+   MUSCLE_NODISCARD uint32 operator () (const QRect & r) const
+   {
+      const uint32 hl = CalculateHashCode(r.left());
+      const uint32 ht = CalculateHashCode(r.top());
+      const uint32 hw = CalculateHashCode(r.width());
+      const uint32 hh = CalculateHashCode(r.height());
+      return (hl + (ht*13) + (hw*39) + (hh*103));
+   }
+
+   /** Returns true iff the two QRect are equal.
+     * @param k1 first key to compare
+     * @param k2 second key to compare
+     */
+   MUSCLE_NODISCARD bool AreKeysEqual(const QRect & k1, const QRect & k2) const {return (k1==k2);}
 };
 
 }  // end namespace muscle
