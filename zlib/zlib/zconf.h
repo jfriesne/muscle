94b370644765b9f310f17532fbda67fadcd7c9b9
Jeremy Friesner <jaf@meyersound.com>
2024-02-15 07:26:33 -0800

    zlib build tweakage

--- zlib/zlib/zconf.h
+++ zlib/zlib/zconf.h
@@ -441,11 +441,11 @@ typedef uLong FAR uLongf;
    typedef unsigned long z_crc_t;
 #endif
 
-#ifdef HAVE_UNISTD_H    /* may be set to #if 1 by ./configure */
+#ifndef WIN32    /* was set to this way by jaf so that ./configure can be optional */
 #  define Z_HAVE_UNISTD_H
 #endif
 
-#ifdef HAVE_STDARG_H    /* may be set to #if 1 by ./configure */
+#if 1    /* was set to #if 1 by ./configure */
 #  define Z_HAVE_STDARG_H
 #endif
 
