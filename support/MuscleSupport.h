0d207cba9e2507d1e4dbb344d16db3f32fc77114
Jeremy Friesner <jaf@meyersound.com>
2025-10-02 09:41:21 -0700

    Added MDEBUG_ON_ERROR and MDEBUG_AND_RETURN_ON_ERROR macros

--- support/MuscleSupport.h
+++ support/MuscleSupport.h
@@ -328,6 +328,18 @@ using std::set_new_handler;
   */
 #define MLOG_AND_RETURN_ON_ERROR(commandDesc, cmd) {const status_t the_return_value = (cmd).GetStatus(); if (the_return_value.IsError()) {LogTime(MUSCLE_LOG_ERROR, "%s:%i:  %s returned [%s]\n", __FILE__, __LINE__, commandDesc, the_return_value()); return the_return_value;}}
 
+/** This macro calls the specified status_t-returning function-call, and if it returns an error-value, writes an error message to the log
+  * @param commandDesc a text string to print to the log if the provided command returns an error code
+  * @param cmd a command to call and test the return value of
+  */
+#define MDEBUG_ON_ERROR(commandDesc, cmd) {const status_t the_return_value = (cmd).GetStatus(); if (the_return_value.IsError()) LogTime(MUSCLE_LOG_DEBUG, "%s:%i:  %s returned [%s]\n", __FILE__, __LINE__, commandDesc, the_return_value());}
+
+/** This macro calls the specified status_t-returning function-call, and if it returns an error-value, writes an error message to the log and then returns the error-value
+  * @param commandDesc a text string to print to the log if the provided command returns an error code
+  * @param cmd a command to call and test the return value of
+  */
+#define MDEBUG_AND_RETURN_ON_ERROR(commandDesc, cmd) {const status_t the_return_value = (cmd).GetStatus(); if (the_return_value.IsError()) {LogTime(MUSCLE_LOG_DEBUG, "%s:%i:  %s returned [%s]\n", __FILE__, __LINE__, commandDesc, the_return_value()); return the_return_value;}}
+
 /** This macro calls the specified status_t-returning function-call, and if it returns an error-value, prints an error message to stdout
   * @param commandDesc a text string to print to stdout if the provided command returns an error code
   * @param cmd a command to call and test the return value of
