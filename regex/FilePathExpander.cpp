0bf57d070049cc44d7741f741664dfe80141e345
Jeremy Friesner <jaf@meyersound.com>
2026-03-25 10:20:02 -0700

    Fixed some minor issues in FilePathExpander.cpp

--- regex/FilePathExpander.cpp
+++ regex/FilePathExpander.cpp
@@ -11,17 +11,18 @@ namespace muscle {
 
 static status_t ExpandFilePathWildCardsAux(const String & curDir, const String & path, Queue<String> & outputPaths, bool isSimpleFormat)
 {
-   const char * fs = GetFilePathSeparator();
+   const char * fs    = GetFilePathSeparator();
+   const uint32 fsLen = (uint32) strlen(fs);  // abject paranoia
    const int32 sepIdx = path.IndexOf(fs);
    const String firstClause  = (sepIdx >= 0) ? path.Substring(0, sepIdx) : path;
-   const String restOfString = (sepIdx >= 0) ? path.Substring(sepIdx+1) : GetEmptyString();
+   const String restOfString = (sepIdx >= 0) ? path.Substring(sepIdx+fsLen) : GetEmptyString();
 
-   StringMatcher sm(firstClause, isSimpleFormat);
    Directory dir(curDir());
    if (dir.IsValid())
    {
       if (CanWildcardStringMatchMultipleValues(firstClause))
       {
+         const StringMatcher sm(firstClause, isSimpleFormat);
          while(1)
          {
             const char * fn = dir.GetCurrentFileName();
@@ -59,9 +60,13 @@ static status_t ExpandFilePathWildCardsAux(const String & curDir, const String &
 
 status_t ExpandFilePathWildCards(const String & path, Queue<String> & outputPaths, bool isSimpleFormat)
 {
-   const char * fs = GetFilePathSeparator();
-   if (path.StartsWith(fs)) return ExpandFilePathWildCardsAux(fs,  path()+1, outputPaths, isSimpleFormat);  // start at root
-                       else return ExpandFilePathWildCardsAux(".", path,     outputPaths, isSimpleFormat);  // start at current folder
+   if (path.IsEmpty()) return B_NO_ERROR;   // nothing to do
+
+   // TODO:  someday handle Windows drive-letter absolute paths (like "C:\\foo\\bar") ?
+   const char * fs    = GetFilePathSeparator();
+   const uint32 fsLen = (uint32) strlen(fs);  // abject paranoia
+   if (path.StartsWith(fs)) return ExpandFilePathWildCardsAux(fs,  path()+fsLen, outputPaths, isSimpleFormat);  // start at root
+                       else return ExpandFilePathWildCardsAux(".", path,         outputPaths, isSimpleFormat);  // start at current folder
 }
 
 } // end namespace muscle
